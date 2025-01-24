7660736e1e0d3743c99cb0d909599023f302690a
Ben Pfaff <blp@feldera.com>
2025-01-23 17:11:13 -0800

    [dbsp] Add async, parallel layer file reader API.
    This adds new `AsyncRowGroup` and `AsyncCursor` structs to the layer file
    reader API.  These are similar to the existing `RowGroup` and `Cursor`
    APIs, respectively, but any methods that could block on I/O are now
    `async` in the new structs.
    
    The new APIs require the caller to provide an `AsyncCacheContext`, which
    takes a collection of `Future`s that use `AsyncCursor` and therefore might
    block on I/O.  `AsyncCacheContext` runs all of the futures until they all
    block or complete.  When all the remaining futures block, it issues their
    I/O in a single async batch.
    
    Nothing uses this new layer file async reader yet.  That will be in the
    next commit.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/trace/spine_async/mod.rs
+++ crates/dbsp/src/trace/spine_async/mod.rs
@@ -27,8 +27,9 @@ pub use crate::trace::spine_async::snapshot::SpineSnapshot;
 use crate::trace::CommittedSpine;
 use enum_map::EnumMap;
 use feldera_storage::StoragePath;
+use futures::{stream::FuturesUnordered, StreamExt};
 use list_merger::ArcMerger;
-use metrics::counter;
+use metrics::{counter};
 use ouroboros::self_referencing;
 use rand::Rng;
 use rkyv::{
@@ -49,10 +50,12 @@ use textwrap::indent;
 
 mod list_merger;
 mod snapshot;
+use self::thread::{BackgroundThread, WorkerStatus};
+
+use super::{cursor::CursorFactory, BatchLocation};
+
 mod thread;
 
-use self::thread::{BackgroundThread, WorkerStatus};
-use super::BatchLocation;
 use crate::circuit::metrics::{BATCHES_PER_LEVEL, COMPACTION_STALL_TIME, ONGOING_MERGES_PER_LEVEL};
 pub use list_merger::ListMerger;
 
@@ -908,6 +911,59 @@ where
                 }
             }
         }
+    }
+
+    async fn fetch<KR>(
+        &self,
+        keys: &KR,
+    ) -> Option<Box<dyn CursorFactory<Self::Key, Self::Val, Self::Time, Self::R>>>
+    where
+        KR: Batch<Key = Self::Key, Time = ()>,
+    {
+        let mut batches = Vec::new();
+        let mut fetched = Vec::new();
+        let mut futures = self
+            .merger
+            .get_batches()
+            .into_iter()
+            .map(|b| async move { (b.clone(), b.fetch(keys).await) })
+            .collect::<FuturesUnordered<_>>();
+        while let Some((batch, fetch)) = futures.next().await {
+            if let Some(fetch) = fetch {
+                fetched.push(fetch);
+            } else {
+                batches.push(batch);
+            }
+        }
+
+        Some(Box::new(Fetch {
+            weight_factory: self.factories.weight_factory(),
+            batches,
+            fetched,
+        }))
+    }
+}
+
+pub struct Fetch<B: Batch> {
+    weight_factory: &'static dyn Factory<B::R>,
+    batches: Vec<Arc<B>>,
+    fetched: Vec<Box<dyn CursorFactory<B::Key, B::Val, B::Time, B::R>>>,
+}
+
+impl<B> CursorFactory<B::Key, B::Val, B::Time, B::R> for Fetch<B>
+where
+    B: Batch,
+{
+    fn get_cursor<'a>(&'a self) -> Box<dyn Cursor<B::Key, B::Val, B::Time, B::R> + 'a> {
+        let cursors =
+            self.fetched
+                .iter()
+                .map(|hc| hc.get_cursor())
+                .chain(self.batches.iter().map(|b| {
+                    Box::new(b.cursor()) as Box<dyn Cursor<B::Key, B::Val, B::Time, B::R>>
+                }))
+                .collect::<Vec<_>>();
+        Box::new(CursorList::new(self.weight_factory, cursors))
     }
 }
 
